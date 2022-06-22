4383e646dee6a080418989d72d4d8dcd019b60a3
taru <kirikia@gmail.com>
2022-06-22 19:01:09 +0900

    update

--- README.md
+++ README.md
@@ -1,22 +1,42 @@
 # openCV_dnn_module_and_YOLO
-OpenCVのDNNモジュールを用いて、YOLOv3 openimagesとScaled YOLOv4を動かすサンプルコード
+OpenCVのDNNモジュールを用いて、USBカメラ映像に対して、YOLOv3 openimages, YOLOv4, Scaled YOLOv4を動かすサンプルコード
 
 # Prerequisites
 - Python ライブラリをインストール
   - opencv-python
   - numpy
-- `models`下に配置
+
+- `models`下に`coco.names`と`openimages.names`を配置
+  - [coco.names](https://github.com/AlexeyAB/darknet/blob/master/data/coco.names)
+  - [openimages.names](https://github.com/AlexeyAB/darknet/blob/master/data/openimages.names)
+
+- `models`下に各モデルのcfgとweightsを配置
   - YOLO-v3 openimages
     - [yolov3-openimages.weights](https://pjreddie.com/media/files/yolov3-openimages.weights)
-    - [yolov3-openimages.cfg](https://github.com/pjreddie/darknet/blob/master/cfg/yolov3-openimages.cfg)
+    - [yolov3-openimages.cfg](https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov3-openimages.cfg)
+
   - YOLOv4
     - YOLOv4
+      - [yolov4.weights](https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights)
+      - [yolov4.cfg](https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4-csp.cfg)
     - YOLOv4-tiny
+      - [yolov4-tiny.weights](https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.weights)
+      - [yolov4-tiny.cfg](https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4-tiny.cfg)
+
+  - Scaled YOLOv4
     - YOLOv4x-mish
       - [yolov4x-mish.weights](https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4x-mish.weights)
       - [yolov4x-mish.cfg](https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4x-mish.cfg)
     - YOLOv4-csp
+      - [yolov4-csp.weights](https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-csp.weights)
+      - [yolov4-csp.cfg](https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4-csp.cfg)
     - YOLOv4-p5
+      - [yolov4-p5.weights](https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-p5.weights)
+      - [yolov4-p5.cfg](https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4-p5.cfg)
     - YOLOv4-p6
       - [yolov4-p6.weights](https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-p6.weights)
       - [yolov4-p6.cfg](https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4-p6.cfg)
+
+# 参考
+- [AlexeyAB/darknet](https://github.com/AlexeyAB/darknet)
+- [ghmagazine/opencv_dl_book](https://github.com/ghmagazine/opencv_dl_book)
--- yolov3_openimages.py
+++ yolov3_openimages.py
@@ -4,7 +4,6 @@ import cv2
 import numpy as np
 
 
-# ファイルからクラスの名前のリストを読み込む関数
 def read_classes(file):
     classes = None
     with open(file, mode='r', encoding="utf-8") as f:
@@ -22,38 +21,19 @@ def get_colors(num):
     return colors
 
 
-def main(
-        model_name='yolov3-openimages',
-        confidence_threshold=0.5,
-        nms_threshold=0.4,
-        cuda_on=False,
-):
-    # キャプチャを開く
+def main(confidence_threshold=0.5, nms_threshold=0.4):
     directory = Path.cwd()
-    # 画像
-    # capture = cv2.VideoCapture(str(directory / 'images' / "dog.jpg"))
-    # USBカメラ
     capture = cv2.VideoCapture(0)
     if not capture.isOpened():
         raise IOError("can't open capture!")
 
-    # モデルを読み込む
-    if model_name == 'yolov3-openimages':
-        size = (608, 608)
-        weights = directory / 'models' / "yolov3-openimages.weights"
-        config = directory / 'models' / "yolov3-openimages.cfg"
-    else:
-        raise IOError("Set correct model name!")
+    size = (608, 608)
+    weights = directory / 'models' / "yolov3-openimages.weights"
+    config = directory / 'models' / "yolov3-openimages.cfg"
 
     model = cv2.dnn_DetectionModel(str(weights), str(config))
-
-    # モデルの推論に使用するエンジンとデバイスを設定する
-    if cuda_on:
-        model.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-        model.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-    else:
-        model.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-        model.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+    model.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+    model.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
     # モデルの入力パラメーターを設定する
     scale = 1.0 / 255.0     # スケールファクター
@@ -65,13 +45,10 @@ def main(
     # NMS（Non-Maximum Suppression）をクラスごとに処理する
     model.setNmsAcrossClasses(False)  # （True: 全体、False: クラスごと）
 
-    # クラスリストとカラーテーブルを取得する
-    if model_name == 'yolov3-openimages':
-        classes = read_classes(str(directory / 'models' / "openimages.names"))
+    classes = read_classes(str(directory / 'models' / "openimages.names"))
     colors = get_colors(len(classes))
 
     while True:
-        # フレームをキャプチャして画像を読み込む
         result, image = capture.read()
         if not result:
             cv2.waitKey(0)
@@ -84,7 +61,6 @@ def main(
         if channels == 4:
             image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
 
-        # オブジェクトを検出する
         class_ids, confidences, boxes = model.detect(
             image, confidence_threshold, nms_threshold
         )
@@ -93,7 +69,6 @@ def main(
         class_ids = np.array(class_ids).flatten()
         confidences = np.array(confidences).flatten()
 
-        # 検出されたオブジェクトを描画する
         for class_id, confidence, box in zip(class_ids, confidences, boxes):
             class_name = classes[class_id]
             color = colors[class_id]
@@ -109,7 +84,6 @@ def main(
                 color, thickness, cv2.LINE_AA
             )
 
-        # 画像を表示する
         cv2.imshow("object detection", image)
         key = cv2.waitKey(1)
         if key == ord('q') or key == 27:
